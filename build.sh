39ae77a80275b281c8586489bda4014e1d19f482
redherring2141 <redherring2141@kaist.ac.kr>
2024-08-09 14:10:33 +0900

    Progress save after CAL paper revision 1st draft and before arrange things

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 rm -rf build
-cmake . -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo -DTCNN_CUDA_ARCHITECTURES=80 -DCMAKE_CUDA_COMPILER=$(which nvcc)
+cmake . -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CUDA_COMPILER=$(which nvcc)
+#cmake . -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo -DTCNN_CUDA_ARCHITECTURES=80 -DCMAKE_CUDA_COMPILER=$(which nvcc)
 cmake --build build --config RelWithDebInfo -j
