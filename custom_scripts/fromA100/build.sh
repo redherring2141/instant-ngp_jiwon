234b4adcbfa0e77bbd434e6f4527879f0f939b8a
redherring2141 <redherring2141@kaist.ac.kr>
2024-08-12 16:07:55 +0900

    Arranged scripts more.

--- custom_scripts/fromA100/build.sh
+++
@@ -1,3 +0,0 @@
-rm -rf build
-cmake . -B build -DCMAKE_BUILD_TYPE=RelWithDebInfo -DTCNN_CUDA_ARCHITECTURES=80 -DCMAKE_CUDA_COMPILER=$(which nvcc) -DNGP_BUILD_WITH_GUI=off
-cmake --build build --config RelWithDebInfo -j
