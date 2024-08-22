b0183fec61c3eb4fe353c274690ecbb6ccf756b4
redherring2141 <redherring2141@kaist.ac.kr>
2024-08-22 15:50:57 +0900

    First draft for Fig6 measurement and scripts.

---
+++ CAL_scripts/Fig6_E2ELatency3DGSvsINGP.sh
@@ -0,0 +1,22 @@
+str1="A100"
+str2="Xavier"
+
+# #Train
+# for DATA in drjohnson playroom train truck bicycle bonsai counter flowers garden kitchen room stump treehill
+# do
+#     #A100
+#     python3 /work6/jiwon/NeRF/CAL_3DGS_rev/instant-ngp/scripts/run.py /work5/jiwon/NeRF/datasets/$DATA --n_steps 30000 --save_snapshot /work5/jiwon/NeRF/models_pretrained/$DATA\_30k/$DATA\_30k.ingp
+# done
+
+#Inference
+for DATA in drjohnson playroom train truck bicycle bonsai counter flowers garden kitchen room stump treehill
+do
+    if [ $1 == $str1 ];
+    then
+        #A100
+        python3 /work6/jiwon/NeRF/CAL_3DGS_rev/instant-ngp/scripts/run.py /work5/jiwon/NeRF/datasets/$DATA --load_snapshot /work5/jiwon/NeRF/models_pretrained/$DATA\_30k/$DATA\_30k.ingp --test_transforms /work5/jiwon/NeRF/datasets/$DATA/transforms_test.json # > /work6/jiwon/NeRF/CAL_3DGS_rev/fig6_A100_20240822/log_ingp_render_drjohnson_30k.txt\
+    else
+        #Xavier-AGX
+        python3 /home/jetson-agx/NeRF/CAL_3DGS_rev/instant-ngp/scripts/run.py /home/jetson-agx/NeRF/datasets/$DATA --load_snapshot /home/jetson-agx/NeRF/models_pretrained/$DATA\_30k/$DATA\_30k.ingp --test_transforms /home/jetson-agx/NeRF/datasets/$DATA/transforms_test.json # > /home/jetson-agx/NeRF/CAL_3DGS_rev/fig6_A100_20240822/log_ingp_render_drjohnson_30k.txt
+    fi
+done
--- CAL_scripts/dataset_format_conversion.sh
+++ CAL_scripts/dataset_format_conversion.sh
@@ -10,12 +10,16 @@
 
 cd /work6/jiwon/NeRF/datasets/DeepBlending/drjohnson
 python3 /work6/jiwon/NeRF/CAL_3DGS_rev/instant-ngp_jiwon/scripts/colmap2nerf.py --colmap_matcher exhaustive --run_colmap --aabb_scale 32 --overwrite
+python3 /work6/jiwon/NeRF/CAL_3DGS_rev/instant-ngp_jiwon/CAL_scripts/transform_divider.py /work6/jiwon/NeRF/datasets/DeepBlending/drjohnson/
 
 cd /work6/jiwon/NeRF/datasets/DeepBlending/playroom
 python3 /work6/jiwon/NeRF/CAL_3DGS_rev/instant-ngp_jiwon/scripts/colmap2nerf.py --colmap_matcher exhaustive --run_colmap --aabb_scale 32 --overwrite
+python3 /work6/jiwon/NeRF/CAL_3DGS_rev/instant-ngp_jiwon/CAL_scripts/transform_divider.py /work6/jiwon/NeRF/datasets/DeepBlending/playroom/
 
-cd /work6/jiwon/NeRF/datasets/Tanks_and_Temples/3dgs/train
-python3 /work6/jiwon/NeRF/CAL_3DGS_rev/instant-ngp_jiwon/scripts/colmap2nerf.py --colmap_matcher exhaustive --run_colmap --aabb_scale 32 --overwrite
+# cd /work6/jiwon/NeRF/datasets/Tanks_and_Temples/3dgs/train
+# python3 /work6/jiwon/NeRF/CAL_3DGS_rev/instant-ngp_jiwon/scripts/colmap2nerf.py --colmap_matcher exhaustive --run_colmap --aabb_scale 32 --overwrite
+# python3 /work6/jiwon/NeRF/CAL_3DGS_rev/instant-ngp_jiwon/CAL_scripts/transform_divider.py
 
-cd /work6/jiwon/NeRF/datasets/Tanks_and_Temples/3dgs/truck
-python3 /work6/jiwon/NeRF/CAL_3DGS_rev/instant-ngp_jiwon/scripts/colmap2nerf.py --colmap_matcher exhaustive --run_colmap --aabb_scale 32 --overwrite
+# cd /work6/jiwon/NeRF/datasets/Tanks_and_Temples/3dgs/truck
+# python3 /work6/jiwon/NeRF/CAL_3DGS_rev/instant-ngp_jiwon/scripts/colmap2nerf.py --colmap_matcher exhaustive --run_colmap --aabb_scale 32 --overwrite
+# python3 /work6/jiwon/NeRF/CAL_3DGS_rev/instant-ngp_jiwon/CAL_scripts/transform_divider.py
